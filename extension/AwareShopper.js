b14190ba2435f0f3ff735f51688e10a1dacb9525
Khyta <koderjulien@gmail.com>
2019-08-31 13:35:38 +0200

    Changed a hell lot of code. Added file reading wich is maybe working

--- extension/AwareShopper.js
+++ extension/AwareShopper.js
@@ -1,5 +1,34 @@
 document.body.style.border = "5px solid green";
 current_url = location.host;
+
+file = 'file:///C:/Users/koder/Desktop/Jugendhackt_2019/AwareShopper/extension/Webseiten.csv';
+function printFile(file) {
+  var reader = new FileReader();
+  reader.onload = function(evt) {
+    console.log(evt.target.result);
+  };
+  reader.readAsText(file);
+}
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                tarr.push(headers[j]+":"+data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+    // alert(lines);
+}
+
 if(current_url == "www.amazon.de"){
-  document.body.style.border = "5px solid red";
+  document.body.style.border = "5px gradient red";
 }
