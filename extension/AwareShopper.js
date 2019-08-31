5d713b3552c1eeeb60a0564c886aa7fe420a1b3b
Khyta <koderjulien@gmail.com>
2019-08-31 11:41:44 +0200

    Changed AwareShopper.js to make a red border at www.amazon.de

--- extension/AwareShopper.js
+++ extension/AwareShopper.js
@@ -1,2 +1,5 @@
 document.body.style.border = "5px solid green";
-document.href.
+current_url = location.host;
+if(current_url == "www.amazon.de"){
+  document.body.style.border = "5px solid red";
+}
