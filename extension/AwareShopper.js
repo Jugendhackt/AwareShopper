a5a2103df344e36d1f4ac0bf379b3eb1e5fe7f9a
Khyta <koderjulien@gmail.com>
2019-08-31 14:33:52 +0200

    Tries to open local CSV File. Still not working

--- extension/AwareShopper.js
+++ extension/AwareShopper.js
@@ -2,13 +2,15 @@ document.body.style.border = "5px solid green";
 current_url = location.host;
 
 file = 'file:///C:/Users/koder/Desktop/Jugendhackt_2019/AwareShopper/extension/Webseiten.csv';
-function printFile(file) {
-  var reader = new FileReader();
-  reader.onload = function(evt) {
-    console.log(evt.target.result);
-  };
-  reader.readAsText(file);
-}
+var txt = '';
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function(){
+  if(xmlhttp.status == 200 && xmlhttp.readyState == 0){
+    txt = xmlhttp.responseText;
+  }
+};
+xmlhttp.open("GET","file",true);
+xmlhttp.send();
 
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
